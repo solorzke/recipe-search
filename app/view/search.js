4eb355c3e01b4f8b4f0bfea1eebf1eab0e26ac51
Kevin Solorzano <solorzke@gmail.com>
2020-02-12 17:47:38 -0500

    Swiper component allows swipe to delete functionality

--- app/view/search.js
+++ app/view/search.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet, SafeAreaView, TextInput, ScrollView, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import AwesomeButtonRick from 'react-native-really-awesome-button/src/themes/rick';
+import Swipeable from 'react-native-swipeable-row';
 
 export default class Search extends Component {
 	state = { data: [] };
@@ -34,15 +35,25 @@ export default class Search extends Component {
 	};
 
 	render() {
+		const rightButtons = [
+			<TouchableOpacity style={styles.delete}>
+				<Text style={{ color: '#fff', fontSize: 15, paddingLeft: 10 }}>Delete</Text>
+			</TouchableOpacity>
+		];
+
 		/* Add a new text input every time a new element is added to the state */
 		let boxes = this.state.data.map((data, index) => {
 			return (
-				<TextInput
-					key={index}
-					placeholder={'Ingredient'}
-					style={styles.input}
-					onChangeText={(text) => this.setState({ text })}
-				/>
+				<Swipeable rightButtons={rightButtons}>
+					<View style={styles.swipableView}>
+						<TextInput
+							key={index}
+							placeholder={'Ingredient'}
+							style={styles.input}
+							onChangeText={(text) => this.setState({ text })}
+						/>
+					</View>
+				</Swipeable>
 			);
 		});
 
@@ -62,11 +73,13 @@ export default class Search extends Component {
 					>
 						Confirm
 					</AwesomeButtonRick>
-					<TextInput
-						placeholder={'Ingredient'}
-						style={styles.input}
-						onChangeText={(text) => this.setState({ text })}
-					/>
+					<View style={styles.swipableView}>
+						<TextInput
+							placeholder={'Ingredient'}
+							style={styles.input}
+							onChangeText={(text) => this.setState({ text })}
+						/>
+					</View>
 					{boxes}
 				</ScrollView>
 			</SafeAreaView>
@@ -82,7 +95,7 @@ const styles = StyleSheet.create({
 
 	area: {
 		flex: 2,
-		margin: 10
+		paddingLeft: 10
 	},
 
 	title: {
@@ -95,10 +108,33 @@ const styles = StyleSheet.create({
 		borderColor: 'gray',
 		borderBottomWidth: 1,
 		height: 50,
-		backgroundColor: '#E8E8E8'
+		backgroundColor: '#E8E8E8',
+		marginVertical: 20,
+		borderWidth: 1
 	},
 
 	confirm: {
 		alignSelf: 'center'
+	},
+
+	swipableView: {
+		borderColor: '#000',
+		borderRadius: 20,
+		borderWidth: 1,
+		height: 100,
+		backgroundColor: '#DDD',
+		marginVertical: 20,
+		marginHorizontal: 20,
+		paddingHorizontal: 50,
+		justifyContent: 'center'
+	},
+
+	delete: {
+		borderColor: 'gray',
+		borderBottomWidth: 1,
+		height: 100,
+		backgroundColor: '#d9534f',
+		marginVertical: 20,
+		justifyContent: 'center'
 	}
 });
