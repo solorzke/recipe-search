57a53bfa5ba390c4f5a47577d8df3804dd6d0827
Kevin Solorzano <solorzke@gmail.com>
2020-02-13 18:00:19 -0500

    Settings page created

--- app/view/settings.js
+++ app/view/settings.js
@@ -1,15 +1,67 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import HeadBar from '../components/head';
+import Status from '../components/statusbar';
 
-class Settings extends Component {
+export default class Settings extends Component {
 	render() {
 		return (
-			<View>
-				<Text>Hello World</Text>
+			<View style={styles.mainView}>
+				<Status color={'#000'} barStyle={'light-content'} />
+				<HeadBar name={'Settings'} onPress={() => this.props.navigation.toggleDrawer()} />
+				<ScrollView style={styles.container}>
+					<TouchableOpacity style={styles.item} onPress={() => this.props.navigation.navigate('Privacy')}>
+						<Text style={styles.title}>Privacy Policy</Text>
+						<Text style={styles.subtitle} />
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.item} onPress={() => this.props.navigation.navigate('Version')}>
+						<Text style={styles.title}>Version</Text>
+						<Text style={styles.subtitle} />
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.item} onPress={() => this.props.navigation.navigate('API')}>
+						<Text style={styles.title}>API</Text>
+						<Text style={styles.subtitle} />
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.item} onPress={() => this.props.navigation.navigate('Terms')}>
+						<Text style={styles.title}>Terms and Conditions</Text>
+						<Text style={styles.subtitle} />
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.item} onPress={() => this.props.navigation.navigate('Terms')}>
+						<Text style={styles.title}>Contact Us</Text>
+						<Text style={styles.subtitle} />
+					</TouchableOpacity>
+				</ScrollView>
 			</View>
 		);
 	}
 }
 
-const styles = StyleSheet.create({});
-export default Settings;
+const styles = StyleSheet.create({
+	mainView: {
+		flex: 1,
+		width: '100%',
+		backgroundColor: '#DDD'
+	},
+
+	container: {
+		flex: 1
+	},
+
+	item: {
+		height: 50,
+		width: '100%',
+		borderBottomWidth: 1,
+		borderColor: '#E8E8E8',
+		flexDirection: 'column'
+	},
+
+	title: {
+		color: '#1976d2',
+		fontSize: 20
+	},
+
+	subtitle: {
+		color: '#f58426',
+		fontSize: 15
+	}
+});
