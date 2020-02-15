7304b3bd50cd05dfec26ed4f1675fe992cf966c4
Kevin Solorzano <solorzke@gmail.com>
2020-02-15 16:46:29 -0500

    Class 'API' defined for sending HTTP-API requests

--- app/api/index.js
+++ app/api/index.js
@@ -1,13 +1,53 @@
-const unirest = require('unirest');
-const api = require('./token');
+/* 
+API Class that can be reused to test or request data from the API in service.
+API: 'edamam'
+HTTP Request Package: 'unirest' 
 
-const API_KEY = api.key;
-const API_ID = api.id;
+Required: 'API KEY' and 'API ID' and <query search>
+*/
+export default class API {
+	constructor(items) {
+		var _API = require('./token');
+		this.getAPIKey = () => {
+			return _API.key;
+		};
+		this.getAPIId = () => {
+			return _API.id;
+		};
+		this.items = items;
+		this.unirest = require('unirest');
+	}
 
-let requestString = 'https://api.edamam.com/search?q=chicken';
-requestString = requestString + '&app_id=' + API_ID + '&app_key=' + API_KEY + '&to=1';
-unirest.get(requestString).end(function(result) {
-	console.log(result.body.hits[0]);
+	requestHTTP = (callback) => {
+		let requestString = 'https://api.edamam.com/search?' + this.returnIngredients() + '&' + this.returnAuth();
+		this.unirest.get(requestString).end((result) => {
+			let data = [];
+			for (let item of result.body.hits) {
+				data.push({
+					label: item['recipe']['label'],
+					image: item['recipe']['image'],
+					source: item['recipe']['source'],
+					url: item['recipe']['url'],
+					dietLabels: item['recipe']['dietLabels'],
+					healthLabels: item['recipe']['healthLabels'],
+					cautions: item['recipe']['cautions'],
+					ingredientLines: item['recipe']['ingredientLines'],
+					calories: item['recipe']['calories'],
+					totalTime: item['recipe']['totalTime'],
+					digest: item['recipe']['digest']
+				});
+			}
+			if (typeof callback === 'function') {
+				callback(data);
+			}
+		});
+	};
 
-	console.log('not here');
-});
+	returnIngredients = () => {
+		return 'q=' + this.items.join();
+	};
+
+	returnAuth = () => {
+		return 'app_id=' + this.getAPIId() + '&app_key=' + this.getAPIKey();
+	};
+}
