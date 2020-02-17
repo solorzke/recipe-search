6ddce7f467f4eb50fc3e1dc84dc3ee89febcfd0a
Kevin Solorzano <solorzke@gmail.com>
2020-02-16 22:09:01 -0500

    Class now uses in-house fetch API for network requests

--- app/api/index.js
+++ app/api/index.js
@@ -1,3 +1,5 @@
+import { call } from 'react-native-reanimated';
+
 /* 
 API Class that can be reused to test or request data from the API in service.
 API: 'edamam'
@@ -15,33 +17,41 @@ export default class API {
 			return _API.id;
 		};
 		this.items = items;
-		this.unirest = require('unirest');
 	}
 
-	requestHTTP = (callback) => {
-		let requestString = 'https://api.edamam.com/search?' + this.returnIngredients() + '&' + this.returnAuth();
-		this.unirest.get(requestString).end((result) => {
-			let data = [];
-			for (let item of result.body.hits) {
-				data.push({
-					label: item['recipe']['label'],
-					image: item['recipe']['image'],
-					source: item['recipe']['source'],
-					url: item['recipe']['url'],
-					dietLabels: item['recipe']['dietLabels'],
-					healthLabels: item['recipe']['healthLabels'],
-					cautions: item['recipe']['cautions'],
-					ingredientLines: item['recipe']['ingredientLines'],
-					calories: item['recipe']['calories'],
-					totalTime: item['recipe']['totalTime'],
-					digest: item['recipe']['digest']
+	async requestHTTP(callback) {
+		try {
+			let requestString = 'https://api.edamam.com/search?' + this.returnIngredients() + '&' + this.returnAuth();
+			await fetch(requestString)
+				.then((response) => {
+					return response.json();
+				})
+				.then((payload) => {
+					let data = [];
+					for (let item of payload.hits) {
+						data.push({
+							label: item['recipe']['label'],
+							image: item['recipe']['image'],
+							source: item['recipe']['source'],
+							url: item['recipe']['url'],
+							dietLabels: item['recipe']['dietLabels'],
+							healthLabels: item['recipe']['healthLabels'],
+							cautions: item['recipe']['cautions'],
+							ingredientLines: item['recipe']['ingredientLines'],
+							calories: item['recipe']['calories'],
+							totalTime: item['recipe']['totalTime'],
+							digest: item['recipe']['digest']
+						});
+					}
+
+					if (typeof callback === 'function') {
+						callback(data);
+					}
 				});
-			}
-			if (typeof callback === 'function') {
-				callback(data);
-			}
-		});
-	};
+		} catch (error) {
+			console.error(error);
+		}
+	}
 
 	returnIngredients = () => {
 		return 'q=' + this.items.join();
