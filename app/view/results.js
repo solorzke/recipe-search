fcd70cbb34aa2baac1013a6bb35f4596aaf11270
Kevin Solorzano <solorzke@gmail.com>
2020-02-21 14:08:41 -0500

    Shorten or abbreviate string with '...'

--- app/view/results.js
+++ app/view/results.js
@@ -3,14 +3,25 @@ import { View, StyleSheet, ScrollView } from 'react-native';
 import ListItem from '../components/listitem';
 
 export default class Results extends Component {
+	sliceString = (str, limit) => {
+		if (str.length > limit) {
+			return str.slice(0, limit - 1) + '...';
+		} else {
+			return str;
+		}
+	};
+
 	render() {
 		const { data } = this.props.route.params;
 		const results = data.map((recipe, index) => {
+			console.warn(recipe['healthLabels']);
+			const labels =
+				recipe['healthLabels'].length !== 0 ? recipe['healthLabels'].join(', ') : 'Delicious & Nutritional';
 			return (
 				<ListItem
 					id={index}
-					title={recipe['label']}
-					subtitle={recipe['totalTime']}
+					title={this.sliceString(recipe['label'], 50)}
+					subtitle={this.sliceString(labels, 60)}
 					img={recipe['image']}
 					calories={Math.ceil(recipe['calories'])}
 					onPress={() => this.props.navigation.navigate('Recipe', { food: recipe })}
