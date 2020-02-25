54b22ce3113999fa9895624248d6769d1846ca28
Kevin Solorzano <solorzke@gmail.com>
2020-02-25 17:11:39 -0500

    Add bookmark options and labels to media options

--- app/components/header.js
+++ app/components/header.js
@@ -10,11 +10,13 @@ const Header = (props) => {
 				<Text style={{ color: '#1976d2' }}>By: {props.source}</Text>
 			</View>
 			<View style={styles.shareView}>
-				<TouchableOpacity>
-					<Icon name={props.bookmark} size={35} color="gray" />
+				<TouchableOpacity onPress={props.bookmarkOptions[0]}>
+					<Icon name={props.bookmarkOptions[1]} size={35} color={props.bookmarkOptions[2]} />
+					<Text style={styles.mediaText}>Save</Text>
 				</TouchableOpacity>
 				<TouchableOpacity>
 					<Icon name={'share'} size={35} color="gray" />
+					<Text style={styles.mediaText}>Share</Text>
 				</TouchableOpacity>
 			</View>
 		</View>
@@ -47,6 +49,12 @@ const styles = StyleSheet.create({
 	title: {
 		fontSize: 25,
 		fontWeight: 'bold'
+	},
+
+	mediaText: {
+		fontSize: 12,
+		color: 'black',
+		alignSelf: 'center'
 	}
 });
 export default Header;
