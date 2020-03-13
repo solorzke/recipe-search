ca18ac257bf44a8a54e86735c3cde0d0d5c0d149
Kevin Solorzano <solorzke@gmail.com>
2020-03-13 16:08:43 -0500

    Make Hamburger icon smaller and action bar slimmer

--- app/components/head.js
+++ app/components/head.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const HeadBar = (props) => {
 	return (
 		<View style={styles.bar}>
 			<TouchableOpacity onPress={props.onPress}>
-				<Icon name="menu" size={35} color="#fff" />
+				<Icon name="menu" size={33} color="#fff" />
 			</TouchableOpacity>
 			<Text style={styles.title}>{props.name}</Text>
 		</View>
@@ -16,14 +16,16 @@ const HeadBar = (props) => {
 const styles = StyleSheet.create({
 	bar: {
 		width: '100%',
-		height: '10%',
+		height: Dimensions.get('window').height / 12,
+		borderBottomColor: '#11508e',
+		borderBottomWidth: 0.5,
 		shadowColor: '#000000',
 		shadowOffset: {
 			width: 0,
 			height: 0.5
 		},
-		shadowRadius: 5,
-		shadowOpacity: 1.0,
+		shadowRadius: 0.5,
+		shadowOpacity: 0.5,
 		elevation: 5,
 		alignItems: 'center',
 		flexDirection: 'row',
