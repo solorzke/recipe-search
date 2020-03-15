2232680f02646a85123cdbe27c46eac367ea82fe
Kevin Solorzano <solorzke@gmail.com>
2020-03-14 22:58:38 -0500

    Divide Recipe info into tab navigation on screen

--- app/view/recipe.js
+++ app/view/recipe.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { StyleSheet, ScrollView, SafeAreaView } from 'react-native';
+import { StyleSheet, ScrollView, SafeAreaView, View, Text } from 'react-native';
 import Header from '../components/recipe/header';
 import IngredientsList from '../components/recipe/ingredientslist';
 import LabelsList from '../components/recipe/labelslist';
-import TabBar from '../components/recipe/tabbar';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
 export default class Recipe extends Component {
 	state = {
@@ -31,34 +31,49 @@ export default class Recipe extends Component {
 
 	render() {
 		const { food } = this.props.route.params;
+		const Tab = createMaterialTopTabNavigator();
+
+		const InstructionsScreen = () => {
+			return (
+				<SafeAreaView style={styles.mainView}>
+					<ScrollView>
+						<Header
+							label={food['label']}
+							source={food['source']}
+							bookmarkOptions={[
+								() => this.toggleBookmark(),
+								this.state.bookmark_name,
+								this.state.bookmark_color
+							]}
+						/>
+						<IngredientsList
+							img={food['image']}
+							ingredients={food['ingredientLines']}
+							source={food['source']}
+						/>
+						<LabelsList
+							dietItems={food['dietLabels']}
+							healthItems={food['healthLabels']}
+							cautionItems={food['cautions']}
+						/>
+					</ScrollView>
+				</SafeAreaView>
+			);
+		};
+
+		const RecipeInfo = () => {
+			return (
+				<View>
+					<Text>Hello</Text>
+				</View>
+			);
+		};
+
 		return (
-			<SafeAreaView style={styles.mainView}>
-				<ScrollView>
-					<TabBar
-						onRecipePress={() => alert('Recipe tab pressed')}
-						onDescPressed={() => alert('Desc tab pressed')}
-					/>
-					<Header
-						label={food['label']}
-						source={food['source']}
-						bookmarkOptions={[
-							() => this.toggleBookmark(),
-							this.state.bookmark_name,
-							this.state.bookmark_color
-						]}
-					/>
-					<IngredientsList
-						img={food['image']}
-						ingredients={food['ingredientLines']}
-						source={food['source']}
-					/>
-					<LabelsList
-						dietItems={food['dietLabels']}
-						healthItems={food['healthLabels']}
-						cautionItems={food['cautions']}
-					/>
-				</ScrollView>
-			</SafeAreaView>
+			<Tab.Navigator>
+				<Tab.Screen name="Instructions" component={InstructionsScreen} />
+				<Tab.Screen name="Information" component={RecipeInfo} />
+			</Tab.Navigator>
 		);
 	}
 }
