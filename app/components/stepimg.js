b6893af6a065ad6e3b0791a3d410288dc73e8941
Kevin Solorzano <solorzke@gmail.com>
2020-03-16 14:53:47 -0500

    Step Number component created

---
+++ app/components/stepimg.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+
+const StepNumber = (props) => {
+	return (
+		<View style={styles.mainView}>
+			<Text style={{ color: '#fff', fontWeight: 'bold' }}>{props.number}</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	mainView: {
+		backgroundColor: '#ffae19',
+		borderRadius: 50,
+		width: 30,
+		height: 30,
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
+});
+
+export default StepNumber;
