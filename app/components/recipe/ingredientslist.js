5f0ed69e18bd20765d4924c16e6018fb23563a54
Kevin Solorzano <solorzke@gmail.com>
2020-03-17 11:49:49 -0500

    Place modal to pinch zoom food img

--- app/components/recipe/ingredientslist.js
+++ app/components/recipe/ingredientslist.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image, Dimensions, Linking } from 'react-native';
+import { View, Text, StyleSheet, Image, Dimensions, Linking, Modal, Animated } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome5';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { PinchGestureHandler, State } from 'react-native-gesture-handler';
 
 export default class List extends Component {
+	state = {
+		modal: true
+	};
+
+	/* Dynamically create Views for each Ingredient Item */
 	produceItems = (items) => {
 		return items.map((item, index) => {
 			<View
@@ -19,7 +26,33 @@ export default class List extends Component {
 		});
 	};
 
+	/* Toggle Modal View when Food Image is clicked */
+	toggleModal = () => {
+		if (this.state.modal) {
+			this.setState({
+				modal: false
+			});
+		} else {
+			this.setState({
+				modal: true
+			});
+		}
+	};
+
+	/* Handles the state change when the gesture is over. */
+	onZoomStateChange = (event) => {
+		/* ACTIVE is used to check whether the event is still active or not */
+		if (event.nativeEvent.oldState === State.ACTIVE) {
+			/* Set the initial scale value when the animation is done. */
+			Animated.spring(this.scale, {
+				toValue: 1,
+				useNativeDriver: true
+			}).start();
+		}
+	};
+
 	render() {
+		/* Dynamically create Views for each Ingredient Item */
 		let items = this.props.ingredients.map((item, index) => {
 			return (
 				<View style={{ flexDirection: 'row', paddingVertical: 5 }}>
@@ -33,17 +66,54 @@ export default class List extends Component {
 				</View>
 			);
 		});
+
+		/* Animate the scaling and handle the onZoomEvent for the PinchGesture on Modal */
+		scale = new Animated.Value(1);
+		onZoomEvent = Animated.event(
+			[
+				{
+					nativeEvent: { scale: this.scale }
+				}
+			],
+			{
+				useNativeDriver: true
+			}
+		);
+
 		return (
 			<View style={styles.mainView}>
 				<Text style={styles.heading}>Ingredients</Text>
 				<View style={styles.listView}>
 					<View style={styles.imgView}>
-						<Image source={{ uri: this.props.img }} style={styles.img} />
+						<TouchableOpacity onPress={() => this.toggleModal()}>
+							<Image source={{ uri: this.props.img }} style={styles.img} />
+						</TouchableOpacity>
 						<Text onPress={() => Linking.openURL(this.props.url)} style={styles.source}>
 							{this.props.source}
 						</Text>
 					</View>
 					<View style={styles.ingredientsList}>{items}</View>
+					<Modal
+						animationType={'slide'}
+						transparent={false}
+						visible={this.state.modal}
+						onRequestClose={() => {
+							console.log('Modal has been closed.');
+						}}
+					>
+						<View style={styles.modal}>
+							<PinchGestureHandler
+								onGestureEvent={this.onZoomEvent}
+								onHandlerStateChange={this.onZoomStateChange}
+							>
+								<Animated.Image source={{ uri: this.props.img }} style={styles.modalImg} />
+							</PinchGestureHandler>
+							<Text style={{ color: '#fff', fontSize: 20 }}>{this.props.source}</Text>
+							<TouchableOpacity onPress={() => this.toggleModal()} style={styles.modalClose}>
+								<Icon name={'close-circle'} size={60} color={'#E0115F'} />
+							</TouchableOpacity>
+						</View>
+					</Modal>
 				</View>
 			</View>
 		);
@@ -63,7 +133,12 @@ const styles = StyleSheet.create({
 
 	ingredientsList: {},
 
-	imgView: {},
+	modalImg: {
+		width: '100%',
+		height: 300,
+		resizeMode: 'contain',
+		transform: [ { scale: 1 } ]
+	},
 
 	img: {
 		width: Dimensions.get('window').width / 1.2,
@@ -86,5 +161,18 @@ const styles = StyleSheet.create({
 		fontWeight: 'bold',
 		color: '#E0115F',
 		paddingHorizontal: 10
+	},
+
+	modal: {
+		flex: 1,
+		alignItems: 'center',
+		backgroundColor: '#211a23',
+		justifyContent: 'center'
+	},
+
+	modalClose: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		paddingTop: 50
 	}
 });
