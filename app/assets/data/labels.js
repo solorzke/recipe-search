ccc52437d45053ee0260b4a399d892fdc7a11037
Kevin Solorzano <solorzke@gmail.com>
2020-05-20 21:17:40 -0500

    Labels for search label page created

---
+++ app/assets/data/labels.js
@@ -0,0 +1,156 @@
+module.exports = {
+	diets: [
+		{
+			label: 'Gluten Free',
+			value: 'gluten free',
+			color: '#fff'
+		},
+		{
+			label: 'Ketogenic',
+			value: 'ketogenic',
+			color: '#fff'
+		},
+		{
+			label: 'Vegetarian',
+			value: 'vegetarian',
+			color: '#fff'
+		},
+		{
+			label: 'Lacto-Vegetarian',
+			value: 'lacto-vegetarian',
+			color: '#fff'
+		},
+		{
+			label: 'Ovo-Vegetarian',
+			value: 'ovo-egetarian',
+			color: '#fff'
+		},
+		{
+			label: 'Vegan',
+			value: 'vegan',
+			color: '#fff'
+		},
+		{
+			label: 'Pescetarian',
+			value: 'pescetarian',
+			color: '#fff'
+		},
+		{
+			label: 'Paleo',
+			value: 'paleo',
+			color: '#fff'
+		},
+		{
+			label: 'Primal',
+			value: 'primal',
+			color: '#fff'
+		},
+		{
+			label: 'Whole30',
+			value: 'whole30',
+			color: '#fff'
+		}
+	],
+
+	meals: [
+		{
+			label: 'Main Course',
+			value: 'main course',
+			color: '#fff'
+		},
+		{
+			label: 'Side Dish',
+			value: 'side dish',
+			color: '#fff'
+		},
+		{
+			label: 'Dessert',
+			value: 'dessert',
+			color: '#fff'
+		},
+		{
+			label: 'Appetizer',
+			value: 'appetizer',
+			color: '#fff'
+		},
+		{
+			label: 'Salad',
+			value: 'salad',
+			color: '#fff'
+		},
+		{
+			label: 'Bread',
+			value: 'bread',
+			color: '#fff'
+		},
+		{
+			label: 'Breakfast',
+			value: 'breakfast',
+			color: '#fff'
+		},
+		{
+			label: 'Soup',
+			value: 'soup',
+			color: '#fff'
+		},
+		{
+			label: 'Beverage',
+			value: 'beverage',
+			color: '#fff'
+		},
+		{
+			label: 'Sauce',
+			value: 'sauce',
+			color: '#fff'
+		},
+		{
+			label: 'Marinade',
+			value: 'marinade',
+			color: '#fff'
+		},
+		{
+			label: 'Fingerfood',
+			value: 'fingerfood',
+			color: '#fff'
+		},
+		{
+			label: 'Snack',
+			value: 'snack',
+			color: '#fff'
+		},
+		{
+			label: 'Drink',
+			value: 'drink',
+			color: '#fff'
+		}
+	],
+
+	cuisines: [
+		'African',
+		'American',
+		'British',
+		'Cajun',
+		'Caribbean',
+		'Chinese',
+		'Eastern European',
+		'European',
+		'French',
+		'German',
+		'Greek',
+		'Indian',
+		'Irish',
+		'Italian',
+		'Japanese',
+		'Jewish',
+		'Korean',
+		'Latin American',
+		'Mediterranean',
+		'Mexican',
+		'Middle Eastern',
+		'Nordic',
+		'Southern',
+		'Spanish',
+		'Thai',
+		'Vietnamese'
+	]
+};
