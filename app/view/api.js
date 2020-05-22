f7b14070de971f3dc728888a597b454f2447cbac
Kevin Solorzano <solorzke@gmail.com>
2020-05-22 09:54:22 -0500

    Remove 'injected' javascript prop for WebView

--- app/view/api.js
+++ app/view/api.js
@@ -2,19 +2,11 @@ import React, { Component } from 'react';
 import { SafeAreaView, StyleSheet, Text } from 'react-native';
 import { WebView } from 'react-native-webview';
 
-const INJECTED_JAVASCRIPT = `(function() {
-    window.ReactNativeWebView.postMessage(JSON.stringify(window.location));
-})();`;
-
 export default class SpoonacularWebView extends Component {
 	render() {
 		return (
 			<SafeAreaView style={styles.mainView}>
-				<WebView
-					source={{ uri: 'https://spoonacular.com/' }}
-					injectedJavaScript={INJECTED_JAVASCRIPT}
-					onMessage={this.onMessage}
-				/>
+				<WebView source={{ uri: 'https://spoonacular.com/' }} onMessage={this.onMessage} />
 			</SafeAreaView>
 		);
 	}
