af99c7f427f8c1948750085734b60bbc8f17b066
Kevin Solorzano <solorzke@gmail.com>
2020-05-21 23:17:17 -0500

    Separate Instruction components to write cleaner code in class

--- app/components/recipe/steps.js
+++ app/components/recipe/steps.js
@@ -3,45 +3,48 @@ import { View, Text, StyleSheet, Linking } from 'react-native';
 import StepNumber from '../stepimg';
 const Scheme = require('../../assets/schemes/scheme');
 
+EmptyInstruction = ({ source }) => {
+	return (
+		<View>
+			<Text style={styles.instructionTitle}>Instruction:</Text>
+			<View style={styles.instructionBlock}>
+				<StepNumber number={1} />
+				<Text style={styles.instructionText}>
+					For more information, check out{' '}
+					<Text style={{ color: '#1976d2' }} onPress={() => Linking.openURL(this.props.url)}>
+						{source}
+					</Text>'s recipe page.
+				</Text>
+			</View>
+		</View>
+	);
+};
+
+Instruction = ({ index, item }) => {
+	return (
+		<View style={styles.instructionBlock} key={item.id}>
+			<StepNumber number={index + 1} />
+			<Text style={styles.instructionText}>{item}</Text>
+		</View>
+	);
+};
+
 export default class Steps extends Component {
 	/* Return a new view that describes every step from the recipe */
 	renderSteps = (steps) => {
 		let data = [];
-		if (steps[0] !== false) {
-			for (let step of steps) {
-				const name = this.retrieveInstructionName(step);
-				const instructions = this.retrieveInstructions(step);
-				data.push(
-					<View>
-						<Text style={styles.instructionTitle}>{name}</Text>
-						{instructions.map((item, index) => {
-							return (
-								<View style={styles.instructionBlock}>
-									<StepNumber number={index + 1} />
-									<Text style={styles.instructionText}>{item}</Text>
-								</View>
-							);
-						})}
-					</View>
-				);
-			}
-			return data;
-		} else {
-			return (
+		if (steps[0] == false) return <EmptyInstruction source={this.props.source} />;
+		for (let step of steps) {
+			const name = this.retrieveInstructionName(step);
+			const instructions = this.retrieveInstructions(step);
+			data.push(
 				<View>
-					<Text style={styles.instructionTitle}>Instruction:</Text>
-					<View style={styles.instructionBlock}>
-						<StepNumber number={1} />
-						<Text style={styles.instructionText}>
-							For more information, check out{' '}
-							<Text style={{ color: '#1976d2' }} onPress={() => Linking.openURL(this.props.url)}>
-								{this.props.source}
-							</Text>'s recipe page.
-						</Text>
-					</View>
+					<Text style={styles.instructionTitle}>{name}</Text>
+					{instructions.map((item, index) => <Instruction index={index} item={item} />)}
 				</View>
 			);
 		}
+		return data;
 	};
 
 	/* Retrieve the instruction name without modifying the existing array */
@@ -51,20 +54,11 @@ export default class Steps extends Component {
 
 	/* Retrieve all the instructions in the array without modifying it */
 	retrieveInstructions = (step) => {
-		if (step.length > 1) {
-			let data = [];
-			step.sort((a, b) => {
-				return a.number - b.number;
-			});
-			for (let i = 1; i < step.length; i++) {
-				if (step[i]['number'] === i) {
-					data.push(step[i]['instruction']);
-				}
-			}
-			return data;
-		} else {
-			return [ 'For more information, check the source.' ];
-		}
+		if (step.length < 1) return [ 'For more information, check the source.' ];
+		let data = [];
+		step.sort((a, b) => a.number - b.number);
+		for (let i = 1; i < step.length; i++) if (step[i]['number'] === i) data.push(step[i]['instruction']);
+		return data;
 	};
 
 	render() {
